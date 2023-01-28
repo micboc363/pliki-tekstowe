b2030e5fcd573aeec3f984c0e9d68e5f76d1f7c7
micboc363 <118609545+micboc363@users.noreply.github.com>
2023-01-28 11:23:27 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,31 +5,55 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-
+//tworzenie dwoch obiektow klasy ksiazka
 	public static void main(String[] args) throws IOException {
 		Ksiazka k1 = new Ksiazka("Bruce Eckel", "Thinking in Java", 2006);
 		Ksiazka k2 = new Ksiazka("Radoslaw Sokol", "ABC ochrony przed wirusami", 2004);
-		
+    Czytelnik c1 = new Czytelnik("Michał", "Bochenek");
+    
+		//plik teskstwowy musi byc opakowany filewriter/bufferem zeby mozna bylo w nim pisac
 		FileWriter fw = new FileWriter("plik.txt");
 		BufferedWriter bw = new BufferedWriter(fw);
-		
+		//zapisywanie do pliku tesktowego
 		bw.write("-------\n");
+    //wywolanie metody toString
 		bw.write(k1.toString());
 		bw.write("-------\n");
+    //tu tez jest wywolywana metoda toString w inny sposob -> szuuka w obiekcie K2 metode toString stad opis
 		bw.write("" + k2);
 		
-		bw.close();
+		bw.close();//metoda zamykania pliku tekstowego w ktorym bylo pisane
+
+    FileWriter fw2 = new FileWriter("czytelnicy.txt");
+		BufferedWriter bw2 = new BufferedWriter(fw2);
 		
-		FileReader fr = new FileReader("plik.txt");
-		BufferedReader br = new BufferedReader(fr);
+		bw2.write("-------\n");
+		bw2.write(c1.toString());
+		
+		bw2.close();
+		
+		FileReader fr = new FileReader("plik.txt");//tu ten plik jest otwierany do czytania
+		BufferedReader br = new BufferedReader(fr);// z zewnatrz opakowywany obiektem klasy BufferedReader aby miec dostep do metofy readLine ktora czyta linijke tekstu (wiersz)
 		
 		while(true) {
 			String wiersz = br.readLine();
-			if(wiersz == null) break;
-			System.out.print(wiersz + "\n");
+			if(wiersz == null) break;//bedzie czytac do momentu 'break' czyli jezeli wiersz wczytany aktualnie z pliku  jest null czyli nie ma wiecej wierszy
+			System.out.print(wiersz + "\n");//wyswietlanie na ekranie
 		}
 		
 		br.close();
+
+    FileReader fr2 = new FileReader("czytelnicy.txt");//tu ten plik jest otwierany do czytania
+		BufferedReader br2 = new BufferedReader(fr2);// z zewnatrz opakowywany obiektem klasy BufferedReader aby miec dostep do metofy readLine ktora czyta linijke tekstu (wiersz)
+		
+		while(true) {
+			String wiersz = br2.readLine();
+			if(wiersz == null) break;//bedzie czytac do momentu 'break' czyli jezeli wiersz wczytany aktualnie z pliku  jest null czyli nie ma wiecej wierszy
+			System.out.print(wiersz + "\n");//wyswietlanie na ekranie
+		}
+		
+		br2.close();
+    
 	}
 }
 
